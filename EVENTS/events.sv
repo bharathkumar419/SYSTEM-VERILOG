e016c0c501e44214ea053720e05b6fbe3e3c9117
bharathkumar419 <bharathkumar63099@gmail.com>
2025-11-07 13:03:41 +0530

    Create event-driven test module in events.sv
    Added initial event-driven test module with display outputs.

--- EVENTS/events.sv
+++ EVENTS/events.sv
@@ -1,1 +1,30 @@
+////////////////////////////events /////////////////////
+/*
+module test;
+event ev;
 
+initial
+	begin 
+		$display("about to drive ctrl after 10 time units");
+		#10;
+		$display("control is driven at time ,t=%t", $time);
+		-> ev;
+	end 
+
+	initial
+		begin 
+		$display("about to drive data");
+		#10;
+		wait(ev.triggered);
+		$display("data is driven at time , t=%t",$time);
+		end 
+endmodule : test 
+
+OUTPUT:
+# run -all
+# about to drive ctrl after 10 time units
+# about to drive data
+# control is driven at time ,t=                  10
+# data is driven at time , t=                  10
+#  quit -f
+*/
